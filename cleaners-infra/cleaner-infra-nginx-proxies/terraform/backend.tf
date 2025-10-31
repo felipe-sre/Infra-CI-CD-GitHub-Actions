9f2197454927cd757d91b9b41e3bb18fb55c6435
Felipe Santana <devops.azure.lfsg@gmail.com>
2025-10-31 18:55:33 -0300

    Refatorando o arquivo backend.tf

--- cleaners-infra/cleaner-infra-nginx-proxies/terraform/backend.tf
+++ cleaners-infra/cleaner-infra-nginx-proxies/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
    backend "s3" {
-     endpoints                    = {
+     endpoint                    = {
        s3 = "https://nyc3.digitaloceanspaces.com"
      }
      region                      = "us-east-1"
@@ -8,9 +8,8 @@ terraform {
      key                         = "cleaner-backend/default/terraform.tfstate"
      skip_credentials_validation = true
      skip_metadata_api_check     = true
-     skip_requesting_account_id  = true
      skip_region_validation      = true
-     use_path_style           = true
+     force_path_style            = true
  }
 }
 
